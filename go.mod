876daa878ddafe0f75b0b9b2f394c9ab8533cc39
Darsler <darsler@pm.me>
2021-04-10 21:21:52 +0800

    Release v0.1.0

---
+++ go.mod
@@ -0,0 +1,5 @@
+module go.mod
+
+go 1.16
+
+require github.com/graphql-go/graphql v0.7.9 // indirect
---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+
+	"github.com/graphql-go/graphql"
+)
+
+func main() {
+	fmt.Println("Go, GraphQL, Hello World")
+
+	fields := graphql.Fields{
+		"hello": &graphql.Field{
+			Type: graphql.String,
+			Resolve: func(p graphql.ResolveParams) (interface{}, error){
+				return "World", nil
+			},
+		},
+	}
+
+	rootQuery := graphql.ObjectConfig{Name: "RootQuery", Fields: fields}
+	schemaConfig := graphql.SchemaConfig{Query: graphql.NewObject(rootQuery)}
+	schema, err := graphql.NewSchema(schemaConfig)
+	if err != nil {
+		log.Fatalf("Failed to create GraphQL schema, error %v", err)	
+	}
+	query := `
+		{
+			hello
+		}
+	`
+
+	params := graphql.Params{Schema: schema, RequestString: query}
+	r := graphql.Do(params)
+
+	if len(r.Errors) > 0 {
+		log.Fatalf("Failed to execute GraphQL operation, errors: &+v", r.Errors)
+	}
+	rJSON, _ := json.Marshal(r)
+	fmt.Printf("%s \n", rJSON)
+}
